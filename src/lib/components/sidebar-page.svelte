eb551fae5d21da8d3f59daae5675022e3472942e
Mantelis <mantasjasikenas@gmail.com>
2024-11-08 17:50:24 +0200

    Update layout padding

--- src/lib/components/sidebar-page.svelte
+++ src/lib/components/sidebar-page.svelte
@@ -16,8 +16,8 @@
 				<Separator orientation="vertical" class="mr-2 h-4" />
 			</div>
 		</header>
-		<div class="flex flex-1 flex-col gap-4 p-4 pt-0">
-			<div class="min-h-[100vh] flex-1 rounded-xl md:min-h-min p-4 border">
+		<div class="flex flex-1 flex-col gap-4 p-2 pt-0">
+			<div class="min-h-[100vh] flex-1 rounded-xl md:min-h-min p-6 border">
 				{@render children()}
 			</div>
 		</div>
