21a9f80d6d5616e509af23c25d6c9b286293ab95
Mantelis <mantasjasikenas@gmail.com>
2024-11-10 17:27:34 +0200

    Fix more icon

--- src/lib/components/more.svelte
+++ src/lib/components/more.svelte
@@ -9,9 +9,7 @@
 	const { class: className, size }: MoreProps = $props();
 </script>
 
-<!--TODO fix icon sizing and position-->
-<svg width={size} height={size} class={className} viewBox="0 0 16 16">
-	<g></g>
+<svg width={size} height={size} class={className} viewBox="0 0 17 16">
 	<path
 		d="M2 6c-1.104 0-2 0.896-2 2s0.896 2 2 2 2-0.896 2-2-0.896-2-2-2zM2 9c-0.551 0-1-0.448-1-1s0.449-1 1-1 1 0.448 1 1-0.449 1-1 1zM8.5 6c-1.104 0-2 0.896-2 2s0.896 2 2 2 2-0.896 2-2-0.896-2-2-2zM8.5 9c-0.551 0-1-0.448-1-1s0.449-1 1-1 1 0.448 1 1-0.449 1-1 1zM15 6c-1.104 0-2 0.896-2 2s0.896 2 2 2 2-0.896 2-2-0.896-2-2-2zM15 9c-0.551 0-1-0.448-1-1s0.449-1 1-1 1 0.448 1 1-0.449 1-1 1z"
 		fill="currentColor"
