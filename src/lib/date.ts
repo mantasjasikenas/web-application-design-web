ed6527716d5a6dc22f31c1b3e6a5e87c1aaf0e6c
Mantelis <mantasjasikenas@gmail.com>
2024-11-12 12:19:10 +0200

    fix: remove 'z' from date formatting

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -3,7 +3,7 @@ import { format, formatRelative, formatDate as zodFormatDate } from 'date-fns';
 const DATE_FORMAT = 'yyyy-MM-dd HH:mm:ss OOOO';
 
 export function formatDate(date: string, formatStr?: string) {
-	return format(date + 'Z', formatStr || DATE_FORMAT);
+	return format(date, formatStr || DATE_FORMAT);
 }
 
 export function formatDateDistance(date: string) {
